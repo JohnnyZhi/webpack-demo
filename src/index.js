5a1b4475cd9eac132fc4a06835889d0855aee23d
Johnny <1015715475@qq.com>
2021-04-21 18:03:09 +0800

    webpack

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import x from './x.js'
+import png from './assets/1.png'
+
+const div = document.getElementById('app')
+ div.innerHTML = `<img src="${png}">`
+
+
+const button = document.createElement('button')
+button.innerHTML = '懒加载'
+button.onclick = ()=> {
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    }, ()=>{
+        console.log('加载错误')
+    })
+}
+
+div.appendChild(button)
+
+
+
+
